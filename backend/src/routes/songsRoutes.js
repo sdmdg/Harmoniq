611f6ec0f3f870146fb680c0b296a363860aa375
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-08-19 06:19:12 +0530

    Create songsRoutes.js

---
+++ backend/src/routes/songsRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import multer from 'multer';
+import { setSong } from "../controllers/songController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+const router = express.Router();
+
+router.post('/upload-song', protect(["admin","listener","artist"]), upload.single('file'), setSong);
+//router.get('/song/:id', protect(["admin","listener","artist"]), getSong);
+
+export default router;
