d70dd9a10c8da89e31cca93a268f5bd4b51fae95
Pawanigamage <pawanih.22@cse.mrt.ac.lk>
2025-08-30 00:36:12 +0530

    update in server.js after adding the route for reports

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
@@ -5,7 +6,7 @@ import albumRoutes from "./routes/albumRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
 import playlistRoutes from "./routes/playlistRoutes.js";
 import songsRoutes from "./routes/songsRoutes.js";
-
+import reportRoutes from "./routes/reportRoutes.js";
 import cors from "cors";
 import morgan from "morgan";
 import pool from "./config/db.js";
@@ -27,7 +28,7 @@ app.use("/api/album", albumRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/playlist", playlistRoutes);
 app.use("/api/songs", songsRoutes);
-
+app.use("/api/reports", reportRoutes);
 const startServer = async () => {
   try {
     // Check database connection
