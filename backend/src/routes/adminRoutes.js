6afda59a828ab2e7cb0b3ea2267e629e6f413c2a
Pawanigamage <pawanih.22@cse.mrt.ac.lk>
2025-09-04 08:15:21 +0530

    update in adminRotes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 import {Router}from 'express';
-import {getDashboardStats}from '../controllers/adminController.js';
+import {getDashboardStats,getRecentActivity,getUsersignupById}from '../controllers/adminController.js';
+
 import{protect} from '../middleware/authMiddleware.js';
 
 const router=Router();
 
 router.get('/dashboard',protect(['admin']),getDashboardStats);
+router.get('/activity',protect(['admin']),getRecentActivity);
+router.get("/usersignup/:id", protect(["admin"]), getUsersignupById);
 export default router;
