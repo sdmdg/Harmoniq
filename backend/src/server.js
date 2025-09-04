d414d49d858b372dc3e88363a02d8dad50e4b8d0
Pawanigamage <pawanih.22@cse.mrt.ac.lk>
2025-09-04 08:17:36 +0530

    modification in the server.js after making the admin recent activity

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,7 @@ import cors from "cors";
 import morgan from "morgan";
 import pool from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -30,6 +31,7 @@ app.use("/api/playlist", playlistRoutes);
 app.use("/api/songs", songsRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/api/admin", adminRoutes);
+
 const startServer = async () => {
   try {
     // Check database connection
