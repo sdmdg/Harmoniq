31a76d816d915b80d968fc6fd456b119a42badb2
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-09-09 01:48:06 +0530

    Update albumRoutes.js

--- backend/src/routes/albumRoutes.js
+++ backend/src/routes/albumRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAlbum, getAlbumsByUserId ,addAlbum  } from "../controllers/albumController.js";
+import { getAlbum, getAlbumsByUserId ,addAlbum ,uploadAlbumArt } from "../controllers/albumController.js";
 import { protect } from "../middleware/authMiddleware.js";
-
+import multer from "multer";
 const router = express.Router();
-
+const upload = multer({ storage: multer.memoryStorage() }); // Use memory storage
 router.get("/get/:album_id", protect(["admin", "listener", "artist"]), getAlbum);
 router.get("/user/:userId", protect(["artist"]), getAlbumsByUserId);
 router.post("/add", protect(["admin", "artist"]), addAlbum);
+router.post("/upload-art", protect(["admin", "artist"]), upload.single("file"), uploadAlbumArt);
 
 export default router;
