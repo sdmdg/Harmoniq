e64c42980ad03d7276406a71551de6825a1c6572
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-09-10 09:22:13 +0530

    Added findSongsByAlbumId  function

--- backend/src/models/Artist.js
+++ backend/src/models/Artist.js
@@ -2,7 +2,7 @@ import db from '../config/db.js';
 
 export const findAlbumsByUserId = async (userId) => {
     try {
-        const query = 'SELECT id, title FROM albums WHERE artist = $1;';
+        const query = 'SELECT id, title,album_art_id FROM albums WHERE artist = $1;';
         const result = await db.query(query, [userId]);
         return result.rows; // return all albums, not just the first
     } catch (error) {
@@ -10,7 +10,16 @@ export const findAlbumsByUserId = async (userId) => {
         throw error;
     }
 };
-
+export const findSongsByAlbumId = async (albumId) => {
+    try {
+        const query = 'SELECT id, title FROM songs WHERE album_id = $1;';
+        const result = await db.query(query, [albumId]);
+        return result.rows; 
+    } catch (error) {
+        console.error('Error finding songs by album ID:', error);
+        throw error;
+    }
+};
 
 export const createAlbum = async ({ title, artist, releaseDate, albumArtId }) => {
     try {
